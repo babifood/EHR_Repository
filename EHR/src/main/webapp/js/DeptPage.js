8e81ffec5242ba75cb4155dd0a8fa3c697beea49
lanwanxin <lanwanxin@163.com>
2018-05-02 16:17:20 +0800

    添加组织管理首页DeptPage.js

---
+++ EHR/src/main/webapp/js/DeptPage.js
@@ -0,0 +1,14 @@
+//初始化
+$(function(){
+	//加载树状菜单
+	loadTissueArchitectureTree();
+});
+/**
+ * ------------------------------------------部门架构树function-------------------------------------------------
+ * @returns
+ */
+//加载部门信息页面的部门架构树状菜单
+function loadTissueArchitectureTree(){
+	//架构树
+	
+}
