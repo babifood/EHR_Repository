4dd520e41932b1d84aee8ce3be23dac6129f64e6
cj-jesse <jobhting@163.com>
2018-05-24 13:44:24 +0800

    1

---
+++ EHR/src/main/java/com/babifood/dao/HolidayDao.java
@@ -0,0 +1,22 @@
+package com.babifood.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.babifood.entity.HolidayEntity;
+
+public interface HolidayDao {
+
+	public int addHoliday(HolidayEntity holidayEntity);
+	
+	public int updateHoliday(HolidayEntity holidayEntity);
+
+	public Integer findHolidayNumByDate(String date);
+
+	public Map<String, Object> findHolidayById(Integer id);
+
+	public int deleteHolidayById(Integer id);
+
+	public List<Map<String, Object>> findHolidayListByDate(String date);
+	
+}
