c004a7828f580a7bbc6e7ed041e3f7b015dba1d0
lanwanxin <lanwanxin@163.com>
2018-08-07 18:14:50 +0800

    犄角旮旯的微调(#^.^#)

--- EHR/src/main/java/com/babifood/control/RewardPunishmentControl.java
+++ EHR/src/main/java/com/babifood/control/RewardPunishmentControl.java
@@ -3,19 +3,12 @@ package com.babifood.control;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.babifood.entity.LoginEntity;
 import com.babifood.entity.RewardPunishmentEntity;
 import com.babifood.service.RewardPunishmentService;
 
